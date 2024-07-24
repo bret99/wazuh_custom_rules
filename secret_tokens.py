66835ecb8aac1cbf08914aa86589d3ebe09e64b1
bret99 <64787351+bret99@users.noreply.github.com>
2024-07-24 09:15:54 +0000

    Update secret_tokens.py

--- secret_tokens.py
+++ secret_tokens.py
@@ -2,4 +2,4 @@ jira_address = "" # insert youp Jira address; example: https://jira.example.com
 confluence_address = "" # insert youp Confluence address; example: https://jira.example.com
 access_token_pass = "DOMAIN_USER_PASSWORD"
 access_token_login = "DOMAIN_USERNAME"
-secret_tokens = ["api_key", "access_token", "password", "docs.google.com", "drive.google.com", "пароль", "access_key", "secret_key"]
+secret_tokens = ["api_key", "access_token", "password", "docs.google.com", "drive.google.com", "пароль", "access_key", "secret_key"] # configure as one like
