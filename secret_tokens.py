dcbb61d2bb595301d11c9816f26fcc07a70bd550
bret99 <64787351+bret99@users.noreply.github.com>
2024-07-24 08:30:13 +0000

    Add files via upload

---
+++ secret_tokens.py
@@ -0,0 +1,4 @@
+jira_address = "" # insert youp Jira address; example: https://jira.example.com
+confluence_address = "" # insert youp Confluence address; example: https://jira.example.com
+access_token_pass = "DOMAIN_USER_PASSWORD"
+access_token_login = "DOMAIN_USERNAME"
