90534d5a9b0e5870f408e5a225073e78b63943a2
bret99 <64787351+bret99@users.noreply.github.com>
2024-07-24 08:36:04 +0000

    Update get_ad_hostnames.sh

--- get_ad_hostnames.sh
+++ get_ad_hostnames.sh
@@ -1,4 +1,4 @@
-cd /root
+cd /usr/local/bin
 python3 get_ad_hostnames.py > /var/ossec/etc/lists/ad_hostnames
 sed -i 's/$/:/' /var/ossec/etc/lists/ad_hostnames
 cat /var/ossec/etc/lists/ad_hostnames | tr '[:lower:]' '[:upper:]' > /var/ossec/etc/lists/ad_hostnames_upper
