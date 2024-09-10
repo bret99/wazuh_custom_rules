924dd3180c061db5f420409f6fcb91f2be0a813c
bret99 <64787351+bret99@users.noreply.github.com>
2024-09-10 08:12:22 +0000

    Update secret_tokens.py

--- secret_tokens.py
+++ secret_tokens.py
@@ -1,5 +1,7 @@
-jira_address = "" # insert youp Jira address; example: https://jira.example.com
-confluence_address = "" # insert youp Confluence address; example: https://jira.example.com
+jira_address = "" # insert your Jira address; example: https://jira.example.com
+confluence_address = "" # insert your Confluence address; example: https://jira.example.com
 access_token_pass = "DOMAIN_USER_PASSWORD"
 access_token_login = "DOMAIN_USERNAME"
 secret_tokens = ["api_key", "access_token", "password", "docs.google.com", "drive.google.com", "access_key", "secret_key"] # configure as one like
+abuseipdb_token = "" # insert your AbuseIPDB API key here; 1000 requests per day, 30000 requests per month
+ip2location_token = "" # insert your IP2location API key here; 30000 requests per month
