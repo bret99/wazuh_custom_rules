8a8f668884779a8cc4c410456493283cb6ca8293
bret99 <64787351+bret99@users.noreply.github.com>
2024-09-11 05:20:19 +0000

    Update secret_tokens.py

--- secret_tokens.py
+++ secret_tokens.py
@@ -5,3 +5,4 @@ access_token_login = "DOMAIN_USERNAME"
 secret_tokens = ["api_key", "access_token", "password", "docs.google.com", "drive.google.com", "access_key", "secret_key"] # configure as one like
 abuseipdb_token = "" # insert your AbuseIPDB API key here; 1000 requests per day, 30000 requests per month
 ip2location_token = "" # insert your IP2location API key here; 30000 requests per month
+wazuh_manager = "" # insert yuor Wazuh manager address here; example: https://10.10.10.34:55000
