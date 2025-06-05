311b6c884c74ff0be7167b7de5de70ac860f2b48
bret99 <64787351+bret99@users.noreply.github.com>
2025-06-05 14:53:30 +0500

    Delete get_jira_secrets_mail_alert.sh

--- get_jira_secrets_mail_alert.sh
+++
@@ -1,3 +0,0 @@
-cd /usr/local/bin
-python3 get_jira_secrets_mail_alert.py
-cat /usr/local/bin/jira_secrets_info.txt | mail -s "Secrets found in Jira " -r "wazuh.agent@example.com" ismonitoring@example.com # Substitute mail      s to actual ones
