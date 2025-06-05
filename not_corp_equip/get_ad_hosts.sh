17dab66b15f971cf9391300137c739ee5959198f
bret99 <64787351+bret99@users.noreply.github.com>
2025-06-05 12:41:43 +0500

    Add files via upload

---
+++ not_corp_equip/get_ad_hosts.sh
@@ -0,0 +1,11 @@
+cd /usr/local/bin
+python3 get_ad_hosts.py > /var/ossec/etc/lists/ad_hostnames
+sed -i 's/$/:/' /var/ossec/etc/lists/ad_hostnames
+cat /var/ossec/etc/lists/ad_hostnames | tr '[:lower:]' '[:upper:]' > /var/ossec/etc/lists/ad_hostnames_upper
+cat /var/ossec/etc/lists/ad_hostnames_upper | tr '[:upper:]' '[:lower:]' > /var/ossec/etc/lists/ad_hostnames_lower
+cat /var/ossec/etc/lists/ad_hostnames_upper > /var/ossec/etc/lists/ad_hostnames
+cat /var/ossec/etc/lists/ad_hostnames_lower >> /var/ossec/etc/lists/ad_hostnames
+rm -f /var/ossec/etc/lists/ad_hostnames_upper
+rm -f /var/ossec/etc/lists/ad_hostnames_lower
+chown root:root /var/ossec/etc/lists/ad_hostnames
+systemctl restart wazuh-manager
