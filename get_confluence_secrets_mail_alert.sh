e994d13b3a53dbdd5e540f6e2f8f6886c49d5cbf
bret99 <64787351+bret99@users.noreply.github.com>
2025-06-05 14:51:19 +0500

    Delete get_confluence_secrets_mail_alert.sh

--- get_confluence_secrets_mail_alert.sh
+++
@@ -1,3 +0,0 @@
-cd /usr/local/bin
-python3 get_confluence_secrets_mail_alert.py
-cat /usr/local/bin/confluence_secrets_info.txt | mail -s "Secrets found in Confluence " -r "wazuh.agent@example.com" ismonitoring@example.com # Substitute mails to actual ones
