28fb1b0719ee55f8a195880bb95662d6583a3bbd
bret99 <64787351+bret99@users.noreply.github.com>
2025-06-05 14:55:50 +0500

    Delete get_tor_exit_nodes.sh

--- get_tor_exit_nodes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -s https://check.torproject.org/exit-addresses | awk '/ExitAddress/ {print $2}' | sort -n > /var/ossec/etc/lists/tor_exit_nodes
-sed -i 's/$/:/' /var/ossec/etc/lists/tor_exit_nodes
-chown wazuh:wazuh /var/ossec/etc/lists/tor_exit_nodes
-systemctl restart wazuh-manager.service
