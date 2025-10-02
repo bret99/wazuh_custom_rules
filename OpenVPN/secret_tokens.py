19917e172c1aee72b2a24bd9fd91128fbdab87a8
bret99 <64787351+bret99@users.noreply.github.com>
2025-10-02 14:03:23 +0500

    Update secret_tokens.py

--- OpenVPN/secret_tokens.py
+++ OpenVPN/secret_tokens.py
@@ -1,7 +1,7 @@
 abuseipdb_token = "" # insert your AbuseIPDB API key here; 1000 requests per day, 30000 requests per month
 ip2location_token = "" # insert your IP2location API key here; 30000 requests per month
 # ====== OPENSEARCH SETTINGS ======
-HOST = 'http://localhost:9200'
+HOST = 'http://localhost:9200' # change if necessary
 INDEX = 'wazuh-alerts*'
 OS_USERNAME = '' # insert actual volume here
 OS_PASSWORD = '' # insert actual volume here
