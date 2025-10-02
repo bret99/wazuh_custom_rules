8cbe3a5609f14480211db6955aa6fa6971343a1a
bret99 <64787351+bret99@users.noreply.github.com>
2025-10-02 14:56:03 +0500

    Create secret_tokens.py

---
+++ vkteams_integration/secret_tokens.py
@@ -0,0 +1,9 @@
+# ====== OPENSEARCH SETTINGS ======
+HOST = 'http://localhost:9200' # change if necessary
+INDEX = 'wazuh-alerts*'
+OS_USERNAME = '' # insert actual volume here
+OS_PASSWORD = '' # insert actual volume here
+# ====== VK TEAMS SETTINGS ======
+VK_TEAMS_TOKEN = '' # insert actual volume here
+VK_TEAMS_CHAT_ID = '' # insert actual volume here
+VK_TEAMS_HOOK_URL = 'https://myteam.mail.ru/bot/v1/messages/sendText'
