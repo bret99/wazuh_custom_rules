9ad3b4f23cc182099fcc9b988c4edbee1d557de5
bret99 <64787351+bret99@users.noreply.github.com>
2025-10-02 14:02:56 +0500

    Update secret_tokens.py

--- OpenVPN/secret_tokens.py
+++ OpenVPN/secret_tokens.py
@@ -1,2 +1,15 @@
 abuseipdb_token = "" # insert your AbuseIPDB API key here; 1000 requests per day, 30000 requests per month
 ip2location_token = "" # insert your IP2location API key here; 30000 requests per month
+# ====== OPENSEARCH SETTINGS ======
+HOST = 'http://localhost:9200'
+INDEX = 'wazuh-alerts*'
+OS_USERNAME = '' # insert actual volume here
+OS_PASSWORD = '' # insert actual volume here
+
+# ====== EMAIL SETTINGS ======
+SMTP_HOST = '' # insert actual volume here
+SMTP_PORT = 587  # 465 for SMTPS (SSL), 587 for STARTTLS
+SMTP_USER = '' # insert actual volume here (email)
+SMTP_PASS = '' # insert actual volume here
+SENDER = '' # insert actual volume here (email)
+DOMAIN = '' # insert actual volume here
