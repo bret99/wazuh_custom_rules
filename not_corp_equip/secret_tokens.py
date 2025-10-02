d1f9e303fd033328479c73aca7d7c61cf8c88292
bret99 <64787351+bret99@users.noreply.github.com>
2025-10-02 14:24:43 +0500

    Create secret_tokens.py

---
+++ not_corp_equip/secret_tokens.py
@@ -0,0 +1,13 @@
+# ====== OPENSEARCH SETTINGS ======
+HOST = 'http://localhost:9200' # change if necessary
+INDEX = 'wazuh-alerts*'
+OS_USERNAME = '' # insert actual volume here
+OS_PASSWORD = '' # insert actual volume here
+
+# ====== EMAIL SETTINGS ======
+SMTP_HOST = '' # insert actual volume here
+SMTP_PORT = 587  # 465 for SMTPS (SSL), 587 for STARTTLS
+SMTP_USER = '' # insert actual volume here (email)
+SMTP_PASS = '' # insert actual volume here
+SENDER = '' # insert actual volume here (email)
+DOMAIN = '' # insert actual volume here
