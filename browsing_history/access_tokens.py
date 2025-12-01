2260b1a56d24b9b85f1cadb46ad077855b1aaa93
bret99 <64787351+bret99@users.noreply.github.com>
2025-12-01 18:33:48 +0500

    Create access_tokens.py

---
+++ browsing_history/access_tokens.py
@@ -0,0 +1,4 @@
+INDEX = 'wazuh-alerts*' # Substitute for actual one
+HOST = 'http://localhost:9200' # Substitute for actual one
+OS_USERNAME = 'admin' # Substitute for actual one
+OS_PASSWORD = '' # Substitute for actual one
