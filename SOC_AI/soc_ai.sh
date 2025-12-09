64ba2cf364fc33e0b387383688815dc60ab956fc
bret99 <64787351+bret99@users.noreply.github.com>
2025-12-09 15:23:39 +0500

    Update soc_ai.sh

--- SOC_AI/soc_ai.sh
+++ SOC_AI/soc_ai.sh
@@ -6,6 +6,7 @@ ansible -i /usr/local/bin/hosts ai_server -m copy -a "src=/tmp/wazuh_events_leve
 
 # Run remote script with AI model
 ansible -i /usr/local/bin/hosts ai_server -m command -a "python3 /usr/local/bin/soc_ai.py /tmp/wazuh_events_level3_10.json" -b -v # wazuh_events_level3_10.json is value of LOG_FILE in secret_tokens.py
+# ansible -i /usr/local/bin/hosts ai_server -m command -a "python3 /usr/local/bin/soc_ai_v2.py --mode global --file /tmp/wazuh_events_level3_10.json" -b -v # run this one to deal with version 2 (upgraded with EMEBEDDINGS)
 
 # Deleting report files
 ansible -i /usr/local/bin/hosts ai_server -m file -a "path=/tmp/wazuh_events_level3_10.json state=absent" -b -v # wazuh_events_level3_10.json is value of LOG_FILE in secret_tokens.py
