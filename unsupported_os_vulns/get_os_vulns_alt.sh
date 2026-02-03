dd114fd86d7574edc0625a6573013010d0d0fe74
bret99 <64787351+bret99@users.noreply.github.com>
2026-02-03 14:20:15 +0500

    Create get_os_vulns_alt.sh

---
+++ unsupported_os_vulns/get_os_vulns_alt.sh
@@ -0,0 +1,6 @@
+trivy filesystem --scanners vuln --skip-dirs /var/log --format json -o /var/log/pre_vulns.json /
+rm -f /var/log/vulns.json
+cat /var/log/pre_vulns.json | jq -c '.Results[] | select(.Vulnerabilities != null) | .Vulnerabilities[]' > /var/log/pre_vulns2.json
+cat /var/log/pre_vulns2.json > /var/log/vulns.json
+rm -f /var/log/pre_vulns.json
+rm -f /var/log/pre_vulns2.json
